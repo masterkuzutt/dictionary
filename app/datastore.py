384b7fc5f0db1e0f8d821ca8e3c3b6a28e0a6ee5
taka <masterkuzutt+github@gmail.com>
2018-01-09 17:06:18 +0900

    edit testcase for datastore

--- app/datastore.py
+++ app/datastore.py
@@ -1,15 +1,24 @@
 import abc
 import re
 import pickle
+import os
+import copy
 from datetime import datetime  as dt
+from collections.abc import Sequence
 
-class AbstractDataStore(metaclass=abc.ABCMeta):
+class AbstractDataStore(Sequence,metaclass=abc.ABCMeta):
     
     def __init__(self):
-        self.dict = []     
+        self.words = {}     
 
     def __iter__(self):
-        return iter(self.dict)
+        return iter(self.words)
+    
+    def __len__(self):
+        return len(self.words)
+
+    def __getitem__(self,word_name):
+        return self.select(word_name)
 
     @abc.abstractmethod
     def load(self,src):
@@ -20,7 +29,11 @@ class AbstractDataStore(metaclass=abc.ABCMeta):
         pass
 
     @abc.abstractmethod
-    def insert(self,data):
+    def add(self,data):
+        pass
+
+    @abc.abstractmethod
+    def select(self,data):
         pass
 
     @abc.abstractmethod
@@ -34,12 +47,15 @@ class AbstractDataStore(metaclass=abc.ABCMeta):
 class PcklDataStore(AbstractDataStore): 
 
     def load(self,src='default.pkl' ):
-        with open(src,mode="rb+") as f :
-            while 1:
-                try:
-                    self.dict.append( pickle.load(f))
-                except EOFError:
-                    break
+        if os.path.isfile(src):
+            with open(src,mode="rb") as f :
+                while 1:
+                    try:
+                        # data = pickle.load(f)
+                        # self.words[data.word_name] = data 
+                        self.words = pickle.load(f)
+                    except EOFError:
+                        break
         return True
 
     def convert_from_csv(self,src):
@@ -49,31 +65,43 @@ class PcklDataStore(AbstractDataStore):
         # with open ( filepath, "r") as f:
         #     for li in f:
         #         mt = re.match(pt, li.rstrip())
-        #         data = DictData(mt.group(1),None,mt.group(2))
+        #         data = WordData(mt.group(1),None,mt.group(2))
         #         self.dict.append(data)
         # [ print(x.word) for x in self.dict ]
 
     def save(self,dst='default.pkl'):
         with open(dst, mode="wb") as f:
-            for data in self.dict:
-                pickle.dump(data, f)
+            pickle.dump(self.words, f)
+        return True
 
-    def update(self,word,data):
-        pass
+    def select(self,word_name):
+        if ( word_name in self.words):
+            return self.words[word_name] 
+        else :
+            return None
 
-    def insert(self,data):
-        self.dict.append(data)
+    def update(self,data):
+        if ( data.word_name in self.words):
+            self.words[data.word_name] = data
+            return True
+        else:
+            return False
 
-    def delete(self,word):
-        for index,data in enumerate(self):
-            if data.word == word:
-                self.dict.pop(index)
+    def add(self,data):
+        # self.words[data.word_name] = copy.copy(data)
+        self.words[data.word_name] = data
+        return True
 
+    def delete(self,word_name):
+        if ( word_name in self.words):
+            del self.words[word_name]
+            return True
+        else :
+            return False
 
-
-class DictData(object):
-    def __init__(self,word, type=None, discription=None,example=None,date=dt.now()):
-        self.word = word
+class WordData(object):
+    def __init__(self,word_name, type=None, discription=None,example=None,date=dt.now()):
+        self.word_name = word_name
         self.type = type
         self.discription = discription
         self.example = example
--- tests/test_datastore.py
+++ tests/test_datastore.py
@@ -1,39 +1,84 @@
 import unittest
+import os 
 from unittest import TestCase
-from app.datastore import PcklDataStore
+from app.datastore import PcklDataStore,WordData
 
 class TestPcklDataStore(TestCase):
+    def setUpClass():
+        pass
+
     def setUp(self):
         self.pkds = PcklDataStore()
 
     def tearDown(self):
+        if os.path.isfile('default.pkl'):
+            os.remove('default.pkl')
+
+    def tearDownClass():
         pass
 
     def test_init(self):
-        self.assertEqual(self.pkds.dict,[])
+        self.assertEqual(len(self.pkds),0)
 
-    def test_iter(self):
-        pass  
-        #[TODO] learn how to write test case for iterator 
+    def test_add(self):
+        self.pkds.load()
+        data = WordData("test") 
+        self.assertTrue(self.pkds.add(data))
+        self.assertEqual(self.pkds[data.word_name],data)
+
+    def test_save(self):
+        self.pkds.load()
+        data = WordData("test") 
+        self.pkds.add(data)
+        self.assertTrue(self.pkds.save())
 
     def test_load(self):
-        self.assertTrue(self.pkds.load())
+        self.pkds.load()
+        self.assertEqual(len(self.pkds),0)
 
-class TestStringMethods(TestCase):
+        data1 = WordData("test1") 
+        self.pkds.add(data1)
+        self.pkds.save()
+        self.pkds.load()
+        self.assertEqual(len(self.pkds),1)
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+        data2 = WordData("test2") 
+        self.pkds.add(data2)        
+        self.pkds.save()
+        self.pkds.load()
+        self.assertEqual(len(self.pkds),2)
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    def test_update(self):  
+        self.pkds.load()
+        data = WordData("test") 
+        self.pkds.add(data)
+        self.pkds.save()
+        self.pkds.load()
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello','world'])
+        data = self.pkds.select("test")
+        data.discription = "test discription"
+        self.pkds.update(data)        
+        self.pkds.save()
+        
+        self.pkds.load()
+        data = self.pkds.select("test")
+        self.assertEqual(data.discription,"test discription")  
 
-        with self.assertRaises(TypeError):
-            s.split(2)
+
+    def test_delete(self):  
+        self.pkds.load()
+        data = WordData("test") 
+        self.pkds.add(data)
+        self.pkds.save()
+        self.pkds.load()
+
+        self.pkds.delete("test")
+        self.pkds.save()
+        
+        self.pkds.load()
+        data = self.pkds.select("test")
+        self.assertEqual(data,None)  
+
 
 # def main():
 #     unittest.main()
